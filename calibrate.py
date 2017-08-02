5be63faccf8074e59642ee21524a73944ea031fd
Chris Grill <cgrill@claytonkendall.com>
2017-08-02 19:45:14 -0400

    First shot at camera calibration
    Add gitignore

---
+++ calibrate.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+images = glob.glob("camera_cal/calibration*.jpg")
+objpoints = []
+imgpoints = []
+gray = []
+for fname in images:
+    img = cv2.imread(fname)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+    if ret == True:
+        imgpoints.append(corners)
+        objpoints.append(objp)
+
+img = cv2.imread("camera_cal/calibration19.jpg")
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imshow("Undist", dst)
+cv2.waitKey()
+
+
+
+
+
